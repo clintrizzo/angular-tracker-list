a6af61a17344f10288df525323636efe5de8c058
Clint <maxeva02@gmail.com>
2021-10-15 02:44:59 -0700

    added a new task in the JSON file

--- angular-tracker-list/src/app/mock-tasks.ts
+++ angular-tracker-list/src/app/mock-tasks.ts
@@ -19,4 +19,10 @@ export const TASKS: Task[] = [
     day: 'May 7th at 12:30pm',
     reminder: false,
   },
+  {
+    id: 4,
+    text: 'Practice Angular',
+    day: 'October 15th at 1:30pm',
+    reminder: true,
+  },
 ];
