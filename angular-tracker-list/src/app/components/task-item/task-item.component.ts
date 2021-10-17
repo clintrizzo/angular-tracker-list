1e9f4bead89bd6460343c96488d3041b276a0f5d
Clint <maxeva02@gmail.com>
2021-10-16 19:32:20 -0700

    updated color for icon

--- angular-tracker-list/src/app/components/task-item/task-item.component.ts
+++ angular-tracker-list/src/app/components/task-item/task-item.component.ts
@@ -9,7 +9,7 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./task-item.component.css']
 })
 export class TaskItemComponent implements OnInit {
-  @Input() task!: Task
+  @Input() task!: Task;
   faTrash = faTrash;
   constructor() { }
 
